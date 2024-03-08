10aead3c6e87cd0e2dcb64b82066263ae732b316
Lucas <lucaslan10@hotmail.com>
2024-03-08 10:53:44 -0300

    Inicio da criação da página de treino

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const botao = document.getElementById("botao");
 
-botao.addEventListener("click", function(e) {
+botao.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
+    const container = document.querySelector(".container")
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
     const description = document.getElementById("description");
@@ -11,19 +12,25 @@ botao.addEventListener("click", function(e) {
 
     let imc = peso /( altura * altura);
 
-    if(imc < 18.5){
-        descricao = "Abaixo do peso";
-    } else if( 18.5 >= imc <= 25) {
-        descricao = "Peso normal";
-    } else if( 25 >= imc <= 30) {
-        descricao = "Acima do peso";
+    if(!imc) {
+        console.log("erro");
     } else {
-        descricao = "Obeso";
+        if(imc < 18.5){
+            descricao = "Abaixo do peso";
+        } else if( 18.5 >= imc <= 25) {
+            descricao = "Peso normal";
+        } else if( 25 >= imc <= 30) {
+            descricao = "Acima do peso";
+        } else {
+            descricao = "Obeso";
+        }
+
+        imcValue.innerText = imc.toFixed(2);
+        description.innerText = descricao;
+    
+        document.getElementById("resultado").classList.remove("hidden");
     }
-
-    imcValue.innerText = imc.toFixed(2);
-    description.innerText = descricao;
-
-    document.getElementById("resultado").classList.remove("hidden");
-
+    
+    // container.style.height = "600px";
+    
 })
